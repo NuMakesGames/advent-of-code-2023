49e05825a3b42236bf5403ea1763f7a5a7e92d6b
Marcus Reid <>
2023-12-19 02:15:52 +0000

    Solve puzzle 18.

--- AdventOfCode/AdventOfCode/Puzzle18A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle18A.cpp
@@ -7,17 +7,82 @@ using namespace Utilities;
 
 namespace Puzzle18A
 {
-	auto ReadInput(const std::filesystem::path& inputFile)
-	{
-		auto input = ReadAllLinesInFile(inputFile);
+	constexpr Vector2d<int> up = { 0, -1 };
+	constexpr Vector2d<int> down = { 0, 1 };
+	constexpr Vector2d<int> left = { -1, 0 };
+	constexpr Vector2d<int> right = { 1, 0 };
 
-		return input;
+	struct DigInstruction
+	{
+		Vector2d<int> direction = up;
+		int distance = 0;
+	};
+
+	std::vector<DigInstruction> ReadInput(const std::filesystem::path& inputFile)
+	{
+		std::vector<DigInstruction> digInstructions;
+		for (const std::string& line : ReadAllLinesInFile(inputFile))
+		{
+			std::vector<std::string> tokens = SplitString(line, " ");
+			DigInstruction digInstruction;
+			switch (tokens[0][0])
+			{
+				case 'R':
+					digInstruction.direction = right;
+					break;
+				case 'L':
+					digInstruction.direction = left;
+					break;
+				case 'U':
+					digInstruction.direction = up;
+					break;
+				case 'D':
+					digInstruction.direction = down;
+					break;
+			}
+			digInstruction.distance = std::stoi(tokens[1]);
+			digInstructions.push_back(digInstruction);
+		}
+
+		return digInstructions;
 	}
 
 	void PrintSolution(const std::filesystem::path& inputFile, bool shouldRender)
 	{
-		auto input = ReadInput(inputFile);
+		std::vector<DigInstruction> digInstructions = ReadInput(inputFile);
 
-		std::cout << "Puzzle18A not yet solved!";
+		// Pick's thoerem solves for area of a polygon with integer vertex coordinates:
+		//   area = interior points + boundary points / 2 - 1
+		//
+		// Boundary points can be computed by counting the points on the dig path and
+		// area can be computed with the shoelace formula. Then:
+		//   interior points = area - boundary points / 2 + 1
+		//
+		// The total number of dug points is the sum of the boundary points and interior points.
+
+		// Find the vertices and the number of boundary points
+		int boundaryPoints = 0;
+		std::vector<Vector2d<int>> vertices;
+		Vector2d<int> currentPosition = { 0, 0 };
+		for (const DigInstruction& digInstruction : digInstructions)
+		{
+			currentPosition += digInstruction.direction * digInstruction.distance;
+			boundaryPoints += digInstruction.distance;
+			vertices.push_back(currentPosition);
+		}
+
+		// Compute the polygon's area using the shoelace formula
+		int area = 0;
+		for (int i = 0; i < vertices.size(); ++i)
+		{
+			int j = (i + 1) % vertices.size();
+			area += vertices[i].x * vertices[j].y - vertices[j].x * vertices[i].y;
+		}
+		area /= 2;
+
+		// Compute interior points using the derivation above
+		int interiorPoints = area - boundaryPoints / 2 + 1;
+
+		std::cout << boundaryPoints + interiorPoints;
 	}
 } // namespace Puzzle18A
--- AdventOfCode/AdventOfCode/Puzzle18B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle18B.cpp
@@ -7,17 +7,83 @@ using namespace Utilities;
 
 namespace Puzzle18B
 {
-	auto ReadInput(const std::filesystem::path& inputFile)
-	{
-		auto input = ReadAllLinesInFile(inputFile);
+	constexpr Vector2d<int64_t> up = { 0, -1 };
+	constexpr Vector2d<int64_t> down = { 0, 1 };
+	constexpr Vector2d<int64_t> left = { -1, 0 };
+	constexpr Vector2d<int64_t> right = { 1, 0 };
 
-		return input;
+	struct DigInstruction
+	{
+		Vector2d<int64_t> direction = up;
+		int64_t distance = 0;
+	};
+
+	std::vector<DigInstruction> ReadInput(const std::filesystem::path& inputFile)
+	{
+		std::vector<DigInstruction> digInstructions;
+		for (const std::string& line : ReadAllLinesInFile(inputFile))
+		{
+			std::vector<std::string> tokens = SplitString(line, " ");
+			DigInstruction digInstruction;
+			switch (tokens[2][tokens[2].size() - 2])
+			{
+				case '0':
+					digInstruction.direction = right;
+					break;
+				case '1':
+					digInstruction.direction = down;
+					break;
+				case '2':
+					digInstruction.direction = left;
+					break;
+				case '3':
+					digInstruction.direction = up;
+					break;
+			}
+			digInstruction.distance = std::stoll(tokens[2].substr(2, 5), nullptr, 16);
+			digInstructions.push_back(digInstruction);
+		}
+
+		return digInstructions;
 	}
 
 	void PrintSolution(const std::filesystem::path& inputFile, bool shouldRender)
 	{
-		auto input = ReadInput(inputFile);
+		std::vector<DigInstruction> digInstructions = ReadInput(inputFile);
 
-		std::cout << "Puzzle18B not yet solved!";
+		// Pick's thoerem solves for area of a polygon with integer vertex coordinates:
+		//   area = interior points + boundary points / 2 - 1
+		//
+		// Boundary points can be computed by counting the points on the dig path and
+		// area can be computed with the shoelace formula. Then:
+		//   interior points = area - boundary points / 2 + 1
+		//
+		// The total number of dug points is the sum of the boundary points and interior points.
+
+		// Find the vertices and the number of boundary points
+		int64_t boundaryPoints = 0;
+		std::vector<Vector2d<int64_t>> vertices;
+		Vector2d<int64_t> currentPosition = { 0, 0 };
+		for (const DigInstruction& digInstruction : digInstructions)
+		{
+			currentPosition += digInstruction.direction * digInstruction.distance;
+			boundaryPoints += digInstruction.distance;
+			vertices.push_back(currentPosition);
+		}
+
+		// Compute the polygon's area using the shoelace formula
+		int64_t area = 0;
+		for (int i = 0; i < vertices.size(); ++i)
+		{
+			int j = (i + 1) % vertices.size();
+			area += vertices[i].x * vertices[j].y - vertices[j].x * vertices[i].y;
+		}
+		area /= 2;
+
+
+		// Compute interior points using the derivation above
+		int64_t interiorPoints = area - boundaryPoints / 2 + 1;
+
+		std::cout << boundaryPoints + interiorPoints;
 	}
 } // namespace Puzzle18B
