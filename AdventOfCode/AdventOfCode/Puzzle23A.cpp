66981118e8301765759d66cf410f86c8c0cd41d0
Marcus Reid <>
2023-12-24 03:08:32 +0000

    Solve puzzle 23.

--- AdventOfCode/AdventOfCode/Puzzle23A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle23A.cpp
@@ -7,17 +7,104 @@ using namespace Utilities;
 
 namespace Puzzle23A
 {
-	auto ReadInput(const std::filesystem::path& inputFile)
-	{
-		auto input = ReadAllLinesInFile(inputFile);
+	constexpr Vector2d<int> up = { 0, -1 };
+	constexpr Vector2d<int> down = { 0, 1 };
+	constexpr Vector2d<int> left = { -1, 0 };
+	constexpr Vector2d<int> right = { 1, 0 };
+	constexpr std::array<Vector2d<int>, 4> directions = { up, down, left, right };
 
-		return input;
-	}
+	struct SearchState
+	{
+		Vector2d<int> current;
+		Grid2d<char> visited;
+	};
 
 	void PrintSolution(const std::filesystem::path& inputFile, bool shouldRender)
 	{
-		auto input = ReadInput(inputFile);
+		Grid2d<char> grid = ReadAllLinesInFileAsGrid(inputFile);
+		Vector2d<int> start{ 1, 0 };
+		Vector2d<int> end{ grid.Width() - 2, grid.Height() - 1 };
+		VerifyElseCrash(grid.at(start) == '.' && grid.at(end) == '.');
+		grid.at(start) = 'S';
 
-		std::cout << "Puzzle23A not yet solved!";
+		// Find the longest path
+		int64_t longestPath = 0;
+		Grid2d<char> longestVisited;
+
+		// Setup a stack to run DFS
+		std::stack<SearchState> toExplore;
+		toExplore.push({ start, grid });
+		while (!toExplore.empty())
+		{
+			auto [current, visited] = toExplore.top();
+			toExplore.pop();
+
+			// If we've reached the end, check if this path is longer than the longest path we've found so far
+			if (current == end)
+			{
+				int64_t visitedCount = std::ranges::count(visited, 'O');
+				if (visitedCount > longestPath)
+				{
+					longestPath = visitedCount;
+					longestVisited = visited;
+				}
+				continue;
+			}
+
+			// Explore all directions
+			for (const Vector2d<int>& direction : directions)
+			{
+				Vector2d<int> next = current + direction;
+				Grid2d<char> nextVisited = visited;
+				if (!nextVisited.IsInBounds(next))
+				{
+					continue;
+				}
+
+				char c = nextVisited.at(next);
+				if (c == '.')
+				{
+					nextVisited.at(next) = 'O';
+					toExplore.push({ next, nextVisited });
+					continue;
+				}
+
+
+				Vector2d<int> downhill = next;
+				switch (c)
+				{
+					case '<':
+						downhill = next + left;
+						break;
+					case '>':
+						downhill = next + right;
+						break;
+					case '^':
+						downhill = next + up;
+						break;
+					case 'v':
+						downhill = next + down;
+						break;
+				}
+
+				// If we're not going down a slope, we're done
+				if (next == downhill)
+				{
+					continue;
+				}
+
+				// If we're going down a slope, check that the downhill position is valid
+				if (!nextVisited.IsInBounds(downhill) || nextVisited.at(downhill) != '.')
+				{
+					continue;
+				}
+
+				nextVisited.at(next) = 'O';
+				nextVisited.at(downhill) = 'O';
+				toExplore.push({ downhill, nextVisited });
+			}
+		}
+
+		std::cout << longestPath;
 	}
 } // namespace Puzzle23A
--- AdventOfCode/AdventOfCode/Puzzle23B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle23B.cpp
@@ -7,17 +7,205 @@ using namespace Utilities;
 
 namespace Puzzle23B
 {
-	auto ReadInput(const std::filesystem::path& inputFile)
-	{
-		auto input = ReadAllLinesInFile(inputFile);
+	constexpr Vector2d<int> up = { 0, -1 };
+	constexpr Vector2d<int> down = { 0, 1 };
+	constexpr Vector2d<int> left = { -1, 0 };
+	constexpr Vector2d<int> right = { 1, 0 };
+	constexpr std::array<Vector2d<int>, 4> directions = { up, down, left, right };
 
-		return input;
+	// Search state for DFS through the graph
+	struct GraphSearchState
+	{
+		int iNodeCurrent = 0;
+		int totalWeight = 0;
+		uint64_t visited = 0;
+	};
+
+	// Edges in the graph
+	struct Edge
+	{
+		int iTargetNode = 0;
+		int weight = 0;
+	};
+
+	// Nodes in the graph
+	struct Node
+	{
+		Vector2d<int> position;
+		std::vector<Edge> edges;
+	};
+
+	// Simple check for how many empty spaces are around a given position; used to find nodes in the graph
+	int CountAdjacentSpaces(const Grid2d<char>& grid, const Vector2d<int>& position)
+	{
+		int count = 0;
+		for (const Vector2d<int>& direction : directions)
+		{
+			Vector2d<int> next = position + direction;
+			if (grid.IsInBounds(next) && grid.at(next) == '.')
+			{
+				++count;
+			}
+		}
+
+		return count;
+	}
+
+	// Reduces search space by merging runs as weighted graph edges
+	std::vector<Node> CompressGridToGraph(const Grid2d<char>& grid, const Vector2d<int>& start, const Vector2d<int>& end)
+	{
+		std::vector<Node> graph;
+		graph.emplace_back(start);
+		graph.emplace_back(end);
+
+		// Find all nodes
+		for (int i = 0; i < grid.size(); ++i)
+		{
+			Vector2d<int> position = grid.GetCoordinatesFromIndex(i);
+			if (grid.at(position) != '.')
+			{
+				continue;
+			}
+
+			if (position == start || position == end)
+			{
+				continue;
+			}
+
+			// Any position with 3 or more adjacent free spaces is a node in the graph
+			if (CountAdjacentSpaces(grid, position) >= 3)
+			{
+				graph.emplace_back(position);
+			}
+		}
+
+		// Find all edges
+		for (int i = 0; i < graph.size(); ++i)
+		{
+			Node& node = graph[i];
+
+			// BFS from the current node to discover all adjacent nodes
+			Grid2d<int> visited(grid.Width(), grid.Height());
+			std::ranges::fill(visited, -1);
+			visited.at(node.position) = 0;
+
+			// Setup initial queue
+			std::queue<std::pair<Vector2d<int>, Vector2d<int>>> toExplore;
+			for (const Vector2d<int>& direction : directions)
+			{
+				toExplore.push({ node.position, direction });
+			}
+			
+			// Run BFS
+			while (!toExplore.empty())
+			{
+				auto [current, direction] = toExplore.front();
+				toExplore.pop();
+
+				Vector2d<int> next = current + direction;
+				if (!grid.IsInBounds(next) || visited.at(next) >= 0 || grid.at(next) != '.')
+				{
+					continue;
+				}
+
+				visited.at(next) = visited.at(current) + 1;
+
+				// If we've found a node, create the edge and stop searching on this path
+				auto nodeIt = std::ranges::find(graph, next, &Node::position);
+				if (nodeIt != graph.end())
+				{
+					int iTargetNode = static_cast<int>(nodeIt - graph.begin());
+
+					// Add edge if we haven't already discovered it
+					if (!std::ranges::any_of(node.edges, [iTargetNode](const Edge& edge) { return edge.iTargetNode == iTargetNode; }))
+					{
+						node.edges.push_back({ iTargetNode, visited.at(next) });
+						graph[iTargetNode].edges.push_back({ i, visited.at(next) });
+					}
+
+					continue;
+				}
+
+				// Continue searching adjacent positions
+				for (const Vector2d<int>& direction : directions)
+				{
+					toExplore.push({ next, direction });
+				}
+			}
+		}
+
+		return graph;
 	}
 
 	void PrintSolution(const std::filesystem::path& inputFile, bool shouldRender)
 	{
-		auto input = ReadInput(inputFile);
+		Grid2d<char> grid = ReadAllLinesInFileAsGrid(inputFile);
 
-		std::cout << "Puzzle23B not yet solved!";
+		// Replace all slopes with normal positions
+		for (char& c : grid)
+		{
+			switch (c)
+			{
+				case '^':
+				case 'v':
+				case '<':
+				case '>':
+					c = '.';
+					break;
+			}
+		}
+
+		// Define start and end positions
+		Vector2d<int> start{ 1, 0 };
+		Vector2d<int> end{ grid.Width() - 2, grid.Height() - 1 };
+		VerifyElseCrash(grid.at(start) == '.' && grid.at(end) == '.');
+
+		std::vector<Node> graph = CompressGridToGraph(grid, start, end);
+
+		// Verify that the graph is small enough to cache visited nodes in a 64-bit bitmask
+		VerifyElseCrash(graph.size() < sizeof(uint64_t) * 8);
+
+		// Indices for start and end nodes
+		int iNodeStart = 0;
+		int iNodeEnd = static_cast<int>(std::ranges::find(graph, end, &Node::position) - graph.begin());
+
+		// Find the longest path
+		int longestPath = 0;
+		uint64_t visited = 0ull;
+
+		// Setup a stack to run DFS
+		std::stack<GraphSearchState> toExplore;
+		toExplore.push({ iNodeStart, 0, visited });
+		while (!toExplore.empty())
+		{
+			auto [iNode, totalWeight, visited] = toExplore.top();
+			toExplore.pop();
+
+			// If we've reached the end, check if this path is longer than the longest path we've found so far
+			if (iNode == iNodeEnd)
+			{
+				longestPath = std::max(totalWeight, longestPath);
+				continue;
+			}
+
+			// Explore all edges
+			for (const Edge& edge : graph[iNode].edges)
+			{
+				int iNextNode = edge.iTargetNode;
+				uint64_t nextVisited = visited;
+
+				// If we've already visited this node, skip it
+				if (nextVisited & (1ull << iNextNode))
+				{
+					continue;
+				}
+
+				// Mark the node visited and continue exploring
+				nextVisited |= 1ull << iNextNode;
+				toExplore.push({ iNextNode, totalWeight + edge.weight, nextVisited });
+			}
+		}
+
+		std::cout << longestPath;
 	}
 } // namespace Puzzle23B
